140315dcc79d2641dc054fe505d32b9cf9788ba9
Paul Slinkard <slinkardpaul50@gmail.com>
2025-08-21 17:12:55 -0500

    Working on the writing to another json. Restructured again.

--- checkResponses.ps1
+++ checkResponses.ps1
@@ -6,10 +6,27 @@ function checkResponses {
         )
             
             $jsonObject = Get-Content -Path ".\Documents\WindowsPowerShell\my_Functions\newProject\smart_responses.json" -Raw | ConvertFrom-Json
-            
+            $data = $jsonObject
+        do {
             
             if ($jsonObject -contains $UserInput) {
-                Write-Host "Go ahead say something original, Loser"
-            }    
+                Write-Host "Go ahead say something original, Loser" -ForegroundColor Green
+            }elseif(-not ($jsonObject -contains $UserInput)) {
+                Write-Host "That's so WEAK!  Try again and put some effort into it." -ForegroundColor Green
+                $data = [PSCustomObject]@{}
+                $data += $UserInput | ConvertTo-Json 
+                Set-Content -Path ".\Documents\WindowsPowerShell\my_Functions\NewProject\new_smart_responses.json" -Value $data
+
+            } 
+
+        
+            #$UserInput = Read-Host "Come on! Say something, Loser!" 
+            While ($true) {
+                checkResponses 
+            }
+        }
+        While($UserInput -ne 'q') {
+            break
+        }
     } 
 
